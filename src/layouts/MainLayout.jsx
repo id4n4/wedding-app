afd4da3ee3b72dccfeb21cdf52db5a867505f52e
david sanchez <nagatiitoo@hotmail.com>
2024-04-23 09:47:01 -0500

    Actualizar estilos del componente MainLayout.jsx y ajustar transiciones

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,9 +3,9 @@ import { motion } from 'framer-motion'
 export const MainLayout = ({ children, ...props }) => {
   return (
     <motion.main
-      initial={{ opacity: 0, y: 50 }}
+      initial={{ opacity: 0, y: 20 }}
       whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 1, ease: 'easeInOut' }}
+      transition={{ duration: 0.8, ease: 'easeInOut' }}
       className='relative z-10 flex flex-col items-center justify-center gap-5 px-10' {...props}>
       {children}
     </motion.main>
