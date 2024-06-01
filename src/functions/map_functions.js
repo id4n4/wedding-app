7b6fe72d84d3afca1ccfd4d78e365f91b5d7c06c
david sanchez <nagatiitoo@hotmail.com>
2024-06-01 11:55:07 -0500

    chore: Update .env file with API key for Google Maps

--- src/functions/map_functions.js
+++ src/functions/map_functions.js
@@ -1,14 +1,29 @@
 import toast from 'react-hot-toast'
 
-export const startRoute = (destino) => {
-  // obtener ubicación actual
-  navigator.geolocation.getCurrentPosition(({ coords }) => {
-    const origin = `${coords.latitude},${coords.longitude}`
-    const destination = `${destino.lat},${destino.lng}`
-    const url = `https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=${destination}`
-    window.open(url, '_blank')
-  }, e => {
-    console.error(e)
-    toast.error('Debes permitir el acceso a tu ubicación para iniciar la ruta')
-  })
+export const startRoute = async (destino) => {
+  navigator.geolocation.getCurrentPosition((position) => successCallback(position, destino), errorCallback)
+}
+
+function successCallback ({ coords }, destino) {
+  const origin = `${coords.latitude},${coords.longitude}`
+  const destination = `${destino.lat},${destino.lng}`
+  const url = `https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=${destination}`
+  window.open(url, '_blank')
+}
+
+function errorCallback (error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      toast.log('El usuario denegó el permiso para la Geolocalización.')
+      break
+    case error.POSITION_UNAVAILABLE:
+      toast.log('La información de la ubicación no está disponible.')
+      break
+    case error.TIMEOUT:
+      toast.log('La solicitud para obtener la ubicación ha caducado.')
+      break
+    case error.UNKNOWN_ERROR:
+      toast.log('Ocurrió un error desconocido.')
+      break
+  }
 }
