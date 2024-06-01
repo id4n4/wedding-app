1318451412612b15801a80bf69b0d073ba2365d5
david sanchez <nagatiitoo@hotmail.com>
2024-06-01 11:20:47 -0500

    feat: Update outfit images in Outfit component
    The outfit images in the Outfit component have been updated to use separate SVGs for men and women. This improves the visual representation of the outfits and provides a more accurate depiction of the clothing options.

--- src/components/outfit/Outfit.jsx
+++ src/components/outfit/Outfit.jsx
@@ -1,12 +1,12 @@
 import { Button } from '@tremor/react'
 import { useState } from 'react'
 import { AiOutlineBgColors } from 'react-icons/ai'
-import OutfitImage from '../../assets/SVGs/outfit.svg'
+import outfitMan from '../../assets/SVGs/outfitMan.svg'
+import outfitWoman from '../../assets/SVGs/outfitWoman.svg'
 import { MainLayout } from '../../layouts/MainLayout'
 import { SectionLayout } from '../../layouts/SectionLayout'
 import { TitleSection } from '../TitleSection'
 import { ImgFlour } from '../generalComponents/ImgFlour'
-import { ImgTemplate } from '../generalComponents/ImgTemplate'
 import { ModalTemplate } from '../generalComponents/ModalTemplate'
 import { ColorPalette } from './components/ColorPalette'
 
@@ -17,9 +17,11 @@ export const Outfit = () => {
       <ImgFlour isLeft />
       <MainLayout>
         <TitleSection title='Vestuario' />
-        <ImgTemplate src={OutfitImage} alt='outfit' />
         <div className='mt-2 space-y-2 text-center text-primary'>
           <h2 className='text-5xl '>Damas:</h2>
+          <div className='flex justify-center'>
+            <img src={outfitWoman} alt='outfitWoman' loading='lazy' decoding='async' className='h-[15rem] min-w-[100px] max-w-[250px]'/>
+          </div>
           <p className='text-2xl font-semibold uppercase'>Vestido Largo</p>
           <p className='text-2xl font-semibold'>Se reserva el color blanco</p>
           <Button icon={AiOutlineBgColors} onClick={() => setIsOpen(true)} className='uppercase'>Paleta de Colores</Button>
@@ -34,6 +36,9 @@ export const Outfit = () => {
         </div>
         <div className='mt-2 space-y-2 text-center text-primary'>
           <h2 className='text-5xl '>Caballeros:</h2>
+          <div className='flex justify-center'>
+            <img src={outfitMan} alt='outfitMan' loading='lazy' decoding='async' className='h-[15rem] min-w-[100px] max-w-[250px]'/>
+          </div>
           <p className='text-2xl font-semibold uppercase'>
             Conjunto guayabera blanca
           </p>
