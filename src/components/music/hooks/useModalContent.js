4571e5c9a89bf8603af238b69a53748947e58f73
david sanchez <nagatiitoo@hotmail.com>
2024-06-12 16:35:50 -0500

    feat: Add search functionality to Music component
    The Music component now includes a search bar that allows users to search for songs. This feature enhances the user experience by providing a convenient way to find and add songs to the playlist.

--- src/components/music/hooks/useModalContent.js
+++ src/components/music/hooks/useModalContent.js
@@ -1,16 +1,25 @@
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import debounce from 'just-debounce-it'
+import { useCallback, useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 
 const PLAYLIST_ID = import.meta.env.VITE_API_SPOTIFY_PLAYLIST
 const API_PLAYLIST = 'https://api.spotify.com/v1/playlists/' + PLAYLIST_ID
 const SPOTIFY_CLIENT_ID = import.meta.env.VITE_API_SPOTIFY_CLIENT_ID
 const SPOTIFY_CLIENT_SECRET = import.meta.env.VITE_API_SPOTIFY_CLIENT_SECRET
+const API_SEARCH = 'https://api.spotify.com/v1/search'
 
 export const useModalContent = () => {
   const [playlistInfo, setPlaylistInfo] = useState({})
   const [isLoading, setIsLoading] = useState(false)
   const [token, setToken] = useState('')
+  const [search, setSearch] = useState('')
+  const [searchResults, setSearchResults] = useState([])
+
+  // #region compareItems
+  const compareItemsSaved = (trackId) => {
+    return playlistInfo?.tracks?.items?.some((item) => item.track.id === trackId)
+  }
 
   // #region getPlaylistInfo
   const getPlaylistInfo = async () => {
@@ -47,6 +56,68 @@ export const useModalContent = () => {
       setIsLoading(false)
     }
   }
+
+  // #region handleSearch
+  const handleSearch = (value) => {
+    setSearch(value)
+    debounceSearch(value.trim())
+  }
+
+  // #region debounceSearch
+  const debounceSearch = useCallback(
+    debounce((value) => {
+      if (value === '') {
+        setSearchResults([])
+        return
+      }
+      searchTrack(value)
+    }
+    , 500),
+    []
+  )
+
+  // #region searchTrack
+  const searchTrack = async (valueToSearch) => {
+    try {
+      const { data } = await axios.get(API_SEARCH, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        params: {
+          q: valueToSearch,
+          type: 'track',
+          offset: 0,
+          limit: 10
+        }
+      })
+      setSearchResults(data?.tracks?.items || [])
+    } catch (error) {
+      console.error(error)
+      toast.error('Error al buscar la canción')
+    }
+  }
+
+  const addTrackToPlaylist = async (trackUri) => {
+    toast.promise(
+      axios.post(API_PLAYLIST + '/tracks', {
+        uris: [trackUri],
+        position: 0
+      }, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }),
+      {
+        loading: 'Agregando canción a la playlist...',
+        success: () => {
+          getPlaylistInfo()
+          return 'Canción agregada a la playlist'
+        },
+        error: 'Error al agregar la canción a la playlist'
+      }
+    )
+  }
+
   useEffect(() => {
     getToken()
   }, [])
@@ -58,6 +129,11 @@ export const useModalContent = () => {
   }, [token])
   return {
     playlistInfo,
-    isLoading
+    isLoading,
+    handleSearch,
+    search,
+    searchResults,
+    compareItemsSaved,
+    addTrackToPlaylist
   }
 }
